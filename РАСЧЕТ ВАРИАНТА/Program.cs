606877710518f6c3bd89ff56ae823c5693c65840
aleksandrkostin7890 <aleksandrkostin7890@gmail.com>
2024-04-06 02:04:16 +0300

    Add project files.

---
+++ РАСЧЕТ ВАРИАНТА/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace РАСЧЕТ_ВАРИАНТА
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 11;
+            int k = N % 6;
+            Console.WriteLine("Вибраний варіант: " + k);
+        }
+    }
+}
